95a760ee44525eb4e3a6ce1897e4a08fe198d405
Balakhnin Daniil <redcollar@MacBook-Pro-Red.local>
2024-07-14 02:10:39 +0300

    init commit

---
+++ sso/cmd/sso/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+	"sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setUpLogger(cfg.Env)
+
+	log.Info("starting application", slog.Any("config", cfg)) // todo clear it
+
+	// TODO: init app
+
+	// TODO: run grpc-server
+}
+
+func setUpLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
